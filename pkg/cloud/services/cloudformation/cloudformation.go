66a398b91a0de54fd3b80645900ecbc38e5414f9
Christian Bianchi <whites11@gmail.com>
2022-04-13 09:40:27 +0200

    fix typo

--- pkg/cloud/services/cloudformation/cloudformation.go
+++ pkg/cloud/services/cloudformation/cloudformation.go
@@ -75,7 +75,7 @@ func (s *Service) DeleteStack(stackName string) error {
 		StackName:                   &stackName,
 	})
 	if err != nil {
-		return err
+		return microerror.Mask(err)
 	}
 
 	_, err = s.cloudFormationClient.DeleteStack(&cloudformation.DeleteStackInput{
